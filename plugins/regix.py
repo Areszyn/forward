de5779f3c55030e1e8f2b8058d8ff072dbf41d42
Susanta Bhandari <89909878+Susantabhandari@users.noreply.github.com>
2024-07-19 22:43:24 +0545

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -210,8 +210,8 @@ async def edit(msg, title, status, sts):
    text = TEXT.format(i.fetched, i.total_files, i.duplicate, i.deleted, i.skip, status, percentage, estimated_total_time, progress)
    if status in ["cancelled", "completed"]:
       button.append(
-         [InlineKeyboardButton('📢 Updates', url='https://t.me/Madflix_Bots'),
-         InlineKeyboardButton('💬 Support', url='https://t.me/MadflixBots_Support')]
+         [InlineKeyboardButton('📢 ', url='https://t.me/waspros'),
+         InlineKeyboardButton('💬 ', url='https://t.me/neprosz')]
          )
    else:
       button.append([InlineKeyboardButton('✖️ Cancel ✖️', 'terminate_frwd')])
